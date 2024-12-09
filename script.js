0e5ad3229fa1e12f38d25a277dc2ff213d263bd7
Lucas <programadorlucas7@gmail.com>
2024-12-09 10:34:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,32 +10,32 @@ function mostraEscondeResposta(id) {
 
     if (resposta.style.display === "none" || resposta.style.display === "") {
         itens.forEach((item, index) => {
-            item.style.display = "none"; 
-            item.style.backgroundColor = ""; 
-            perguntas[index].style.backgroundColor = ""; 
-            perguntas[index].style.color = ""; 
-            document.getElementById(`numero${index + 1}`).style.color = ""; 
-            cards[index].classList.remove("active"); 
+            item.style.display = "none";
+            item.style.backgroundColor = "";
+            perguntas[index].style.backgroundColor = "";
+            perguntas[index].style.color = "";
+            document.getElementById(`numero${index + 1}`).style.color = "";
+            cards[index].classList.remove("active");
             document.getElementById(`icon${index + 1}`).textContent = "+";
         });
 
         resposta.style.display = "block";
-        resposta.style.backgroundColor = "#1766EF"; 
-        resposta.style.color = "#fff"; 
+        resposta.style.backgroundColor = "#1766EF";
+        resposta.style.color = "#fff";
 
-        icon.textContent = "-"; 
-        pergunta.style.backgroundColor = "#1766EF"; 
+        icon.textContent = "-";
+        pergunta.style.backgroundColor = "#1766EF";
         pergunta.style.color = "#fff";
         numeroCard.style.color = "#fff";
-        cards[id - 1].classList.add("active"); 
+        cards[id - 1].classList.add("active");
     } else {
-        resposta.style.display = "none"; 
-        icon.textContent = "+"; 
+        resposta.style.display = "none";
+        icon.textContent = "+";
 
-        pergunta.style.backgroundColor = ""; 
-        pergunta.style.color = ""; 
-        numeroCard.style.color = ""; 
-        cards[id - 1].classList.remove("active"); 
+        pergunta.style.backgroundColor = "";
+        pergunta.style.color = "";
+        numeroCard.style.color = "";
+        cards[id - 1].classList.remove("active");
     }
 }
 
